90b8bad738bed5c22b3ee64ef14f2f468c71ee20
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-08-22 17:50:46 +0530

    Add files via upload

---
+++ Prog2.java
@@ -0,0 +1,11 @@
+public class Prog2 {
+    public static void main(String args[])
+    {
+        int x=25, i=15,k;
+        double y;
+        k=x+i;
+        y=k/30;
+        System.out.println("Value of k="+k);
+        System.out.println("Value of y="+y);
+    }
+}
