6bdad203815c1c09312d453e918b47299d57a3f0
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-08-22 17:51:41 +0530

    Add files via upload

---
+++ Prog3.java
@@ -0,0 +1,12 @@
+public class Prog3 {
+    public static void main(String args[])
+    {
+         int x=30, y=10;
+         {
+            System.out.println("Sum="+(x+y));
+            System.out.println("Diff="+(x-y));
+            System.out.println("Product="+(x*y));
+            System.out.println("Modulo="+(x/y));
+         }
+    }
+}
