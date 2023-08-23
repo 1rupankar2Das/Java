720fa93a08e25e0528f23ef3ff8b58c5250fe4b2
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-08-23 23:14:54 +0530

    Add files via upload

---
+++ Prog5.java
@@ -0,0 +1,11 @@
+public class Prog5 {  
+        public static void main (String args[])
+        {
+            int n = 13;
+            if(n%2==0)
+                 System.out.println(n+"is Even");
+                 else
+                 System.out.println(n+"is odd");
+        }
+    
+}
