bfee7cb2d4d99f5d62096285b4854b9051f513f6
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-08-23 23:14:21 +0530

    Add files via upload

---
+++ Prog4.java
@@ -0,0 +1,10 @@
+public class Prog4 {
+    public static void main(String args[])
+    {
+        int age=20;
+        if(age>18)
+             System.out.println("Age greater than 18");
+             else
+             System.out.println("Age is not greater than 18");
+    }
+}
