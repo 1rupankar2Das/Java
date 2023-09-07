e47cb606748642aa29be528d18d98d8683773bfd
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-09-07 16:53:45 +0530

    Add files via upload

---
+++ Prog10.java
@@ -0,0 +1,9 @@
+public class Prog10{
+    public static void main(String args[])
+    {
+        int number=24;
+    //using ternary operater
+    String output=(number%2==0)?"Even number":"Odd number";
+    System.out.println("Result--"+output);
+    }
+}
