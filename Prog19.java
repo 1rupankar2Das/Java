087e6b5f17ba4b91c1f01fda651f2b7cf2095eb0
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2023-09-07 17:12:47 +0530

    Add files via upload

---
+++ Prog19.java
@@ -0,0 +1,12 @@
+//Java for-each loop example which prints the
+//elements of the array
+public class Prog19{
+    public static void main(String args[]){
+        //Declaring an array
+        int arr[]={12,23,44,56,78};
+        //Printing array using for-each loop
+        for(int i:arr){
+            System.out.println((i*5));
+        }
+    }
+}
